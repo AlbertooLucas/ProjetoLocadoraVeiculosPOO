0253cd3034722b03d37649bf91d98b98a819ab7c
SeveroGabriel <bielsevero@gmail.com>
2023-03-25 19:26:13 -0300

    Criada a classe a classe Veiculo junto com seus atributos e métodos

--- src/Veiculo.java
+++ src/Veiculo.java
@@ -5,4 +5,61 @@ public class Veiculo {
     private int ano;
     private double diaria;
     private boolean disponivel;
+
+    public Veiculo(String placa, String modelo, String marca, int ano, double diaria, boolean disponivel) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.ano = ano;
+        this.diaria = diaria;
+        this.disponivel = disponivel;
+    }
+
+    public String getPlaca() {
+        retrurn placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public double getDiaria() {
+        return diaria;
+    }
+
+    pubnlic void setDiaria(double diaria) {
+        this.diaria = diaria;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        thjisdisponivel = disponivel;
+    }
 }
