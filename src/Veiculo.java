705dcb5974b94e45c6c67060b8eb295abca2b853
SeveroGabriel <bielsevero@gmail.com>
2023-03-25 18:46:08 -0300

    Criada as classes

---
+++ src/Veiculo.java
@@ -0,0 +1,8 @@
+public class Veiculo {
+    private String placa;
+    private String Modelo;
+    private String marca;
+    private int ano;
+    private double diaria;
+    private boolean disponivel;
+}
